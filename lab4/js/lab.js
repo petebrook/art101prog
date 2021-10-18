86581a7e56e32373eaabca66b224cf53d21a0357
petebrook <91556675+petebrook@users.noreply.github.com>
2021-10-18 12:49:19 -0700

    Latest

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// Summary.
+// Description.
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+// Wake up
+  // Grab the apple bag
+    // Go to the orchard
+      // Find a tree with ripe fruit
+        // Fill the bag
+          // Return to garage and fill boxes with apples
+            // Donate apples at food pantries
