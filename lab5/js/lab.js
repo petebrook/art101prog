05385878ad9ab919a53591c4fb3eedf771010476
petebrook <91556675+petebrook@users.noreply.github.com>
2021-10-18 13:12:54 -0700

    Update lab.js

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+* Author: Pete Brook <pebrook@ucsc.edu>
+* Created: 17 October
+* License: Public Domain
+*/
+
+// Define Variables
+make = "Kia";
+model = "Rio";
+color = "Banana Slug Yellow";
+year = "2006";
+
+//calculate
+age = 2021 - year;
+
+// output
+document.writeLn("Make: " + make + "<br>");
+document.writeLn("Model: " + model + "<br>");
+document.writeLn("Color: " + color + "<br>");
+document.writeLn("Year: " + year + "<br>");
+document.writeLn("Age: " + age + "years<br>");
