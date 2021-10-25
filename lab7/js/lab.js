21b56c65977de0ad6d60b64921c6fa29cc65379d
petebrook <91556675+petebrook@users.noreply.github.com>
2021-10-25 15:07:49 -0700

    Removed the if function

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -4,29 +4,24 @@
  * No copyright.
  **/
 
+ //Sort the letters of the user's name and return those from the function. Hint: You can't sort strings, but you can sort arrays.
 function sortUserName() {
-var userName = window.prompt("What's your name? I can fix it!");
+  var userName = window.prompt("What's your name? I can fix it!");
 
-if (userName != null)
-  document.getElementById("demo").innerHTML =
-  "Hello " + userName + "! How are you today?";
-}
+  console.log("userName =", userName);
 
-console.log("userName =", userName);
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
 
-var nameArray = userName.split('');
-console.log("nameArray =", nameArray);
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
 
-var nameArraySort = nameArray.sort();
-console.log("nameArraySort =", nameArraySort);
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
 
-var nameSorted = nameArraySort.join('');
-console.log("nameSorted =", nameSorted);
-
-return nameSorted;
+  return nameSorted;
 
 }
-//Sort the letters of the user's name and return those from the function. Hint: You can't sort strings, but you can sort arrays.
 
 document.writeln("Hello, here's your new moniker: ", sortUserName());
 
