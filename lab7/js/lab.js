228adda254c9b82c3f21dbf5f4ed66160d13d262
petebrook <91556675+petebrook@users.noreply.github.com>
2021-10-25 14:20:16 -0700

    After troubleshooting with Section A

---
+++ lab7/js/lab.js
@@ -0,0 +1,27 @@
+/**
+ * Author:    Pete
+ * Created:   10.24.2021
+ * No copyright.
+ **/
+
+function sortUserName() {
+var userName = window.prompt("What's your name? I can fix it!");
+console.log("userName =", userName);
+
+var nameArray = userName.split('');
+console.log("nameArray =", nameArray);
+
+var nameArraySort = nameArray.sort();
+console.log("nameArraySort =", nameArraySort);
+
+var nameSorted = nameArraySort.join('');
+console.log("nameSorted =", nameSorted);
+
+return nameSorted;
+
+}
+//Sort the letters of the user's name and return those from the function. Hint: You can't sort strings, but you can sort arrays.
+
+document.writeln("Hello, here's your new moniker: ", sortUserName());
+
+//Convert it back and forth
