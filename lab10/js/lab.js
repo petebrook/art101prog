70e28121da9fec22c34c07cf00e4196c04a970a0
petebrook <91556675+petebrook@users.noreply.github.com>
2021-11-03 13:58:07 -0700

    Nov 3rd

---
+++ lab10/js/lab.js
@@ -0,0 +1,27 @@
+
+/**
+ * Author:    Pete
+ * Created:   11.03.2021
+ * No copyright.
+ **/
+
+ //Sort the letters of the user's name and return those from the function. Hint: You can't sort strings, but you can sort arrays.
+function sortUserName() {
+  var userName = window.prompt("What's your name? I can fix it!");
+
+  console.log("userName =", userName);
+
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+
+  return nameSorted;
+
+  }
+
+  document.writeln("Hello, here's your new moniker: ", sortUserName());
