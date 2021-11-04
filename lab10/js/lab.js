8a778974a91d4cd092c5b9cb994c9a4ec6e3d2c8
petebrook <91556675+petebrook@users.noreply.github.com>
2021-11-04 08:28:38 -0700

    Changes before public embarassment

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -7,8 +7,9 @@
 
  //Sort the letters of the user's name and return those from the function. Hint: You can't sort strings, but you can sort arrays.
 function sortUserName() {
-  var userName = window.prompt("What's your name? I can fix it!");
+  var userName = window.prompt("PLEASE TELL ME YOUR NAME!");
 
+input
   console.log("userName =", userName);
 
   var nameArray = userName.split('');
@@ -24,4 +25,33 @@ function sortUserName() {
 
   }
 
-  document.writeln("Hello, here's your new moniker: ", sortUserName());
+  // Create an event handler within the JS file that listens for a button click.
+  var buttonEl = document.getElementById("mybutton");
+  buttonEl.addEventListener("click", function(){
+    // When the button is pushed,
+    // prompt the user to input their name with the prompt() function.
+    var name = prompt("Please enter your name:");
+
+    // Use this input to change the text from ‘Hello, World!’ to ‘Hello’ + the user’s name
+    var headingEl = document.getElementById("title");
+    headingEl.innerHTML = "Hello, " + name;
+
+  })
+
+  function putNameInTitle() {
+  // When the button is pushed,
+  // prompt the user to input their name
+  // with the prompt() function.
+  //var name = prompt("Please enter your name:");
+  var field = document.getElementById("myfield");
+  var name = field.value;
+
+  // Use this input to change the text from
+  // ‘Hello, World!’ to ‘Hello’ + the user’s name
+  var headingEl = document.getElementById("title");
+  headingEl.innerHTML = "Hello, " + name;
+}
+
+// Create an event handler within the JS file that listens for a button click.
+var buttonEl = document.getElementById("mybutton");
+buttonEl.addEventListener("click", putNameInTitle);
