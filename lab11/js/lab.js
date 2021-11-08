c9c149e6af9281af9764fc0b92ac67fa954075e9
petebrook <91556675+petebrook@users.noreply.github.com>
2021-11-08 14:40:48 -0800

    jQuery fix CSS fix

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -10,8 +10,32 @@
 // Give it a text label
 // Add it to the section
 
-$("#challenges").append("<button id='r-button'>Tempting button</button>");
+$("#challenges").append("<button id='r-button1'>Tempting button</button>");
 
-$("#problems").append("<button id='r-button'>Another tempting button</button>");
+$("#problems").append("<button id='r-button2'>Another tempting button</button>");
 
-$("#results").append("<button id='r-button'>A third tempting button</button>");
+$("#results").append("<button id='r-button3'>A third tempting button</button>");
+
+// Using jQuery:
+// Add a click event to each button
+// Find the challenge, problems, or results section the button is in.
+// Toggle a class special within the <div> of the section
+
+// here's a different maybe better way to do it
+// (we are traversing the DOM to the parent)
+
+$("#r-button1").click(function(){
+  $(this).parent().toggleClass("special");
+})
+
+$("#r-button2").click(function(){
+  $(this).parent().toggleClass("special");
+})
+
+$("#r-button3").click(function(){
+  $(this).parent().toggleClass("special");
+})
+
+// Go into your css and style each of the classes
+
+// Test each button
