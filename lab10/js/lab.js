4dfa0bf91af604c1b6b13a65ecfb7eab4f064169
petebrook <91556675+petebrook@users.noreply.github.com>
2021-11-08 13:59:37 -0800

    JQuery

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -6,35 +6,28 @@
  **/
 
  //Sort the letters of the user's name and return those from the function. Hint: You can't sort strings, but you can sort arrays.
-function sortUserName() {
-  var userName = window.prompt("PLEASE TELL ME YOUR NAME!");
-
-input
-  console.log("userName =", userName);
-
-  var nameArray = userName.split('');
-  console.log("nameArray =", nameArray);
-
-  var nameArraySort = nameArray.sort();
-  console.log("nameArraySort =", nameArraySort);
-
-  var nameSorted = nameArraySort.join('');
-  console.log("nameSorted =", nameSorted);
-
-  return nameSorted;
-
-  }
+// function sortUserName() {
+//   var userName = window.prompt("PLEASE TELL ME YOUR NAME!");
+//
+// input
+//   console.log("userName =", userName);
+//
+//   var nameArray = userName.split('');
+//   console.log("nameArray =", nameArray);
+//
+//   var nameArraySort = nameArray.sort();
+//   console.log("nameArraySort =", nameArraySort);
+//
+//   var nameSorted = nameArraySort.join('');
+//   console.log("nameSorted =", nameSorted);
+//
+//   return nameSorted;
+//
+//}
 
   // Create an event handler within the JS file that listens for a button click.
   var buttonEl = document.getElementById("mybutton");
   buttonEl.addEventListener("click", function(){
-    // When the button is pushed,
-    // prompt the user to input their name with the prompt() function.
-    var name = prompt("Please enter your name:");
-
-    // Use this input to change the text from ‘Hello, World!’ to ‘Hello’ + the user’s name
-    var headingEl = document.getElementById("title");
-    headingEl.innerHTML = "Hello, " + name;
 
   })
 
---
+++ lab11/js/lab.js
@@ -0,0 +1,17 @@
+/**
+ * Author:    Pete
+ * Created:   11.08.2021
+ *
+ * (c) Copyright by blah blah blah
+ **/
+
+// Find the section you want to add the button to
+// Create the button
+// Give it a text label
+// Add it to the section
+
+$("#challenges").append("<button id='r-button'>Tempting button</button>");
+
+$("#problems").append("<button id='r-button'>Another tempting button</button>");
+
+$("#results").append("<button id='r-button'>A third tempting button</button>");
